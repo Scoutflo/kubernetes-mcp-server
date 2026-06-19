79e30f15966ede89e08f1e3152f53057b1dc17f6
Atharva Bondre <atharva@scoutflo.com>
2026-06-19 21:17:03 +0530

    fix(prometheus): send rule query unix times as strings

--- pkg/kubernetes/prometheus.go
+++ pkg/kubernetes/prometheus.go
@@ -3,6 +3,7 @@ package kubernetes
 import (
 	"fmt"
 	"net/url"
+	"strconv"
 	"strings"
 	"time"
 
@@ -248,10 +249,10 @@ func (k *Kubernetes) GetPrometheusRules(groupLimit int, ruleNames, ruleGroups, f
 	}
 
 	if startTime != nil && !startTime.IsZero() {
-		reqBody["start_time"] = startTime.Format(time.RFC3339)
+		reqBody["start_time"] = strconv.FormatInt(startTime.Unix(), 10)
 	}
 	if endTime != nil && !endTime.IsZero() {
-		reqBody["end_time"] = endTime.Format(time.RFC3339)
+		reqBody["end_time"] = strconv.FormatInt(endTime.Unix(), 10)
 	}
 
 	// Make API request to K8s Dashboard
